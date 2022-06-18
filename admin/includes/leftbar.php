8325aed9cd032e0567504181e5b694aa9ac83926
Abhinav Verma <abhinavverma1614@gmail.com>
2022-06-18 23:06:54 +0530

    Website done

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,24 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Courses</a>
+<ul>
+<li><a href="create-brand.php">Create Course</a></li>
+<li><a href="manage-brands.php">Manage Course</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i>Course Content</a>
+					<ul>
+						<li><a href="post-avehical.php">Post a Content</a></li>
+						<li><a href="manage-vehicles.php">Manage Content</a></li>
+					</ul>
+				</li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
+
+			
+
+			</ul>
+		</nav>
